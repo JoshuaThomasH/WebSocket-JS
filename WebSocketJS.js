3d5fe4407ca52efea9639801d9ac92edadbd4770
Joshua <jhavenga@intuiface.com>
2020-06-05 11:42:01 -0500

    Quick change

--- WebSocketJS.js
+++ WebSocketJS.js
@@ -15,9 +15,8 @@
  * Except as contained in this notice, the name of Intuilab shall not be used in advertising or otherwise to promote the sale,
  * use or other dealings in this Software without prior written authorization from Intuilab.
  */
-/**
- * Created by Seb Meunier on 16/07/2015.
- */
+
+
 
 /**
  * Inheritance on EventEmitter base class
